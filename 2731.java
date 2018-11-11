7b8dbfd1ce884d7ff3af3d067bbbb8428cc73b88
AnderBalles98 <44950106+AnderBalles98@users.noreply.github.com>
2018-11-11 13:40:00 -0500

    Update 2731.java

--- 2731.java
+++ 2731.java
@@ -1,13 +1,13 @@
-/**
- * @author ANDERSON BALLESTEROS RODRIGUEZ
- */
- 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
 
- class Main {
+/**
+ * @author ANDERSON BALLESTEROS
+ */
+
+class Main {
 
     public static void main(String[] args) throws IOException {
         BufferedReader leer = new BufferedReader(new InputStreamReader(System.in));
